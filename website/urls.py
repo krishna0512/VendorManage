c7e0458f197578648b70b8a2671c5d22db96c61a
Krishna Tulsyan <kt.krishna.tulsyan@gmail.com>
2020-05-04 18:59:08 +0530

    removed the expert url

--- website/urls.py
+++ website/urls.py
@@ -20,6 +20,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('', include('expert.urls')),
-    path('expert/', include('expert.urls')),
+    # path('expert/', include('expert.urls')),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
