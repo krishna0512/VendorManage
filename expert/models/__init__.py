efe7895320074ce2313c23a44b7abaa152ee0a5b
Krishna Tulsyan <kt.krishna.tulsyan@gmail.com>
2020-05-23 15:06:21 +0530

    completed the integration of kit model inside the kit app

--- expert/models/__init__.py
+++ expert/models/__init__.py
@@ -1,5 +1,3 @@
 from .product import Product
 from challan.models import Challan
 from invoice.models import Invoice
-# from kit.models import Kit, kit_image_path
-from .kit import Kit, kit_image_path
