303f8f98fbf9dace175240aa8320b666e7020d03
Krishna Tulsyan <kt.krishna.tulsyan@gmail.com>
2020-05-23 14:58:07 +0530

    saved

--- expert/models/__init__.py
+++ expert/models/__init__.py
@@ -2,4 +2,4 @@ from .product import Product
 from challan.models import Challan
 from invoice.models import Invoice
 # from kit.models import Kit, kit_image_path
-# from .kit import Kit, kit_image_path
+from .kit import Kit, kit_image_path
