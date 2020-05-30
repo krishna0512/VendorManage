51ab63852f8031a47ff495e72a0798533d230284
Krishna Tulsyan <kt.krishna.tulsyan@gmail.com>
2020-05-30 17:06:12 +0530

    added some more tests for challan models

--- challan/tests/test_models.py
+++ challan/tests/test_models.py
@@ -25,3 +25,34 @@ class ChallanModelTest(TestCase):
         self.assertEqual(field.verbose_name, 'Date of Dispatch')
         self.assertTrue(field.null)
         self.assertFalse(field.blank)
+
+    def test_field_invoice(self):
+        field = self.challan._meta.get_field('invoice')
+        self.assertTrue(field.null)
+        self.assertTrue(field.blank)
+        self.assertIsNone(field.default)
+        self.assertEqual(field.related_name, 'challans')
+
+    def test_field_invoice_on_delete(self):
+        field = self.challan._meta.get_field('invoice')
+        invoice = Invoice.objects.create(number=10)
+        invoice.add_challan(self.challan)
+        self.assertEqual(self.challan.invoice, invoice)
+        invoice.delete()
+        self.assertEqual(self.challan.invoice, field.default)
+
+    def test_field_customer(self):
+        field = self.challan._meta.get_field('customer')
+        self.assertTrue(field.null)
+        self.assertFalse(field.blank)
+        self.assertIsNone(field.default)
+        self.assertEqual(field.related_name, 'challans')
+
+    def test_field_customer_on_delete(self):
+        field = self.challan._meta.get_field('customer')
+        customer = Customer.objects.create()
+        self.challan.customer = customer
+        self.challan.save()
+        self.assertEqual(self.challan.customer, customer)
+        customer.delete()
+        self.assertEqual(self.challan.customer, field.default)
